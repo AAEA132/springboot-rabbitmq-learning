cd4d79921e485d95ec64638a79f2b704e593af65
M. E.A <meab132>
2024-06-28 02:56:45 +0330

    first commit

---
+++ src/main/java/com/m_ea/springboot_rabbitmq_learning/SpringbootRabbitmqLearningApplication.java
@@ -0,0 +1,13 @@
+package com.m_ea.springboot_rabbitmq_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m_ea/springboot_rabbitmq_learning/SpringbootRabbitmqLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m_ea.springboot_rabbitmq_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
